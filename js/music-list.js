8a0c13e936b95f778526eadaa0b1c594c6b20b6e
le tien dat <letiendat3110@gmail.com>
2021-12-23 22:15:02 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,64 @@
+let allMusic = [
+  {
+    name: "Freaky Squad",
+    artist: "Rhymastic, Binz, Soobin, Touliver",
+    img: "song1",
+    src: "song1"
+  },
+  {
+    name: "Cưa Là Đổ",
+    artist: "Phát Hồ & X2X",
+    img: "song2",
+    src: "song2"
+  },
+  {
+    name: "Em Thích",
+    artist: "Sean & Lửa",
+    img: "song3",
+    src: "song3"
+  },
+  {
+    name: "Có Chắc Yêu Là Đây",
+    artist: "Sơn Tùng MTP",
+    img: "song4",
+    src: "song4"
+  },
+  {
+    name: "Đế Vương",
+    artist: "Đình Dũng",
+    img: "song5",
+    src: "song5"
+  },
+  {
+    name: "Biết Tìm Đâu",
+    artist: "Tuấn Hưng",
+    img: "song6",
+    src: "song6"
+  },
+  {
+    name: "3107 - 1",
+    artist: "W/n ft. Nâu, Duongg",
+    img: "song7",
+    src: "song7"
+  },
+  {
+    name: "Ghé Qua",
+    artist: "Dick x PC x Tofu",
+    img: "song8",
+    src: "song8"
+  },
+  {
+    name: "Muộn Rồi Mà Sao Còn",
+    artist: "Sơn Tùng MTP",
+    img: "song9",
+    src: "song9"
+  },
+  {
+    name: "Một Triệu Like",
+    artist: "Đen ft Thành Đồng",
+    img: "song10",
+    src: "song10"
+  },
+  
+ 
+];
